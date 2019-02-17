36924ff4d0f74aef8609c94c5db6846af3d4cc1e
Apodeus <ordonez.romain@gmail.com>
2019-02-17 12:08:49 +0100

    Replace H2 database with postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO voiture VALUES(1, 'renault', 'clio', 2000, CURRENT_TIMESTAMP());
-INSERT INTO voiture VALUES(2, 'opel', 'corsa', 3500, CURRENT_TIMESTAMP());
-INSERT INTO voiture VALUES(3, 'peugeot', '208', 1800, CURRENT_TIMESTAMP());
+INSERT INTO voiture VALUES(1, 'renault', 'clio', 2000, CURRENT_TIMESTAMP);
+INSERT INTO voiture VALUES(2, 'opel', 'corsa', 3500, CURRENT_TIMESTAMP);
+INSERT INTO voiture VALUES(3, 'peugeot', '208', 1800, CURRENT_TIMESTAMP);
